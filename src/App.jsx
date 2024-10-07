7d28bf5104b660a94cf00b7755f217e7aef3f9d6
Varick Tecson <varicktecson@gmail.com>
2024-10-07 09:40:32 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* Your form component or other components will go here */}
+    </div>
+  );
+}
+
+export default App;
